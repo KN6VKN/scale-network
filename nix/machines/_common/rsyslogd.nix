b31b69f19376104f4f10644d2052359f416eb24b
Robert James Hernandez <rob@sarcasticadmin.com>
2024-09-22 19:21:29 +0000

    nix: remove host config from rsyslogd.nix

--- nix/machines/_common/rsyslogd.nix
+++ nix/machines/_common/rsyslogd.nix
@@ -5,20 +5,12 @@
   ...
 }:
 {
-
-  boot.kernelParams = [ "console=ttyS0" ];
-
   networking = {
     firewall.allowedTCPPorts = [ 514 ];
   };
 
-  # TODO: How to handle sudo esculation
-  security.sudo.wheelNeedsPassword = false;
-
   environment.systemPackages = with pkgs; [
     rsyslog
-    vim
-    git
   ];
 
   # Easy test of the service using logger
